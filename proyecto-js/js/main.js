d7479459bf0838cf342860412e571a5fcfc25742
fili258-oss <marinorubianobotina@gmail.com>
2022-06-27 19:28:11 -0500

    Creación slider

---
+++ proyecto-js/js/main.js
@@ -0,0 +1,10 @@
+$(document).ready(function () {
+    $(function () {
+        $('.galeria').bxSlider({
+            mode: 'horizontal',
+            captions: true,
+            slideWidth: 1200,
+            responsive: true
+        });
+    });
+})
